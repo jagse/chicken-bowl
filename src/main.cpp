d16f990b2750c74d88c23984eb44504039612179
Andreas Robecke <andreas@robecke.de>
2025-11-22 19:47:31 +0100

    Pumping

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const int HEATER_RELAY_PIN = 5;  // Digital pin for relay control
+const int HEATER_PIN = 5;  // Digital pin for relay control
 const int PUMP_PIN = 7;  // Digital pin for water pump control
 const int TEMP_SENSOR_PIN = 3;  // Digital pin for DS18B20 temperature sensor
 const float TEMP_TOO_LOW = 10;  
@@ -12,16 +12,22 @@ const int TEN_SECONDS = 10000;  // 10 seconds in milliseconds
 const unsigned long TEMP_READ_INTERVAL = TEN_SECONDS * 6;
 unsigned long lastTempRead = 0;
 
+// Pump configuration
+const unsigned long PUMP_INTERVAL = TEN_SECONDS * 6;  // How often to pump in millis
+const unsigned long PUMP_DURATION = TEN_SECONDS;  // How long to pump each time in millis
+unsigned long lastPumpStart = 0;
+bool pumpRunning = false;
+
 OneWire oneWire(TEMP_SENSOR_PIN);
 DallasTemperature tempSensor(&oneWire);
 
 void heaterOn() {
-  digitalWrite(HEATER_RELAY_PIN, HIGH);
+  digitalWrite(HEATER_PIN, HIGH);
   digitalWrite(LED_BUILTIN, HIGH);
 }
 
 void heaterOff() {
-  digitalWrite(HEATER_RELAY_PIN, LOW);
+  digitalWrite(HEATER_PIN, LOW);
   digitalWrite(LED_BUILTIN, LOW);
 }
 
@@ -38,20 +44,22 @@ float readTemperature() {
   return tempSensor.getTempCByIndex(0);
 }
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(LED_BUILTIN, OUTPUT);
-  pinMode(HEATER_RELAY_PIN, OUTPUT);
-  pinMode(PUMP_PIN, OUTPUT);
-  tempSensor.begin();
-  heaterOff();  // Ensure heat pad is OFF at startup
-  pumpOff();  // Ensure pump is OFF at startup
+void runPump(unsigned long currentMillis) {
+  if (!pumpRunning && (currentMillis - lastPumpStart >= PUMP_INTERVAL)) {
+    // Start pumping
+    pumpOn();
+    pumpRunning = true;
+    lastPumpStart = currentMillis;
+    Serial.println("Pump started");
+  } else if (pumpRunning && (currentMillis - lastPumpStart >= PUMP_DURATION)) {
+    // Stop pumping after duration
+    pumpOff();
+    pumpRunning = false;
+    Serial.println("Pump stopped");
+  }
 }
 
-void loop() {
-  unsigned long currentMillis = millis();
-
+void runHeating(unsigned long currentMillis) {
   if (currentMillis - lastTempRead >= TEMP_READ_INTERVAL) {
     lastTempRead = currentMillis;
     float temp = readTemperature();
@@ -67,4 +75,22 @@ void loop() {
       Serial.println("Heater OFF");
     }
   }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(HEATER_PIN, OUTPUT);
+  pinMode(PUMP_PIN, OUTPUT);
+  tempSensor.begin();
+  heaterOff();  // Ensure heat pad is OFF at startup
+  pumpOff();  // Ensure pump is OFF at startup
+}
+
+void loop() {
+  unsigned long currentMillis = millis();
+
+  runPump(currentMillis);
+  runHeating(currentMillis);
 }
